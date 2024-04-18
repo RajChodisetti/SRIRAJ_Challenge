bfe55daa93b64e2533d30edd00648195b93cb59b
RajChodisetti <110764922+RajChodisetti@users.noreply.github.com>
2024-04-18 13:02:11 -0500

    Update Route53_config.tf

--- Route53_config.tf
+++ Route53_config.tf
@@ -5,3 +5,15 @@ resource "aws_route53_record" "cert_validation" {
   records = [aws_acm_certificate.cert.domain_validation_options.0.resource_record_value]
   ttl     = 60
 }
+resource "aws_route53_health_check" "example" {
+  fqdn              = aws_cloudfront_distribution.s3_distribution.domain_name
+  port              = 443
+  type              = "HTTPS"
+  resource_path     = "/"
+  failure_threshold = 3
+  request_interval  = 30
+
+  tags = {
+    Name = "Endpoint Health Check"
+  }
+}
